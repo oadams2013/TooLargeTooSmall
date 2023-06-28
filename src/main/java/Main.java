d4c54ff6407f132be120b232fc9f6459a261f535
Olamide <olamide@Zipcoders-MacBook-Pro-26.local>
2023-06-28 19:31:58 -0400

    I did it lol

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,52 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random num = new Random();
+
+
+        System.out.println("Welome to Guess The Number!");
+        System.out.println("Guess a Number Below!");
+
+       int n= num.nextInt(20)+1;
+       int g = 0;
+       int tries = 0;
+
+
+        while (n != g) {
+
+            g= scan.nextInt();
+        // if they don't guess right they have to guess again
+            // ex. if they guess 12 but n= 20 then its too small
+            // if they guess 16 but n= 15 then its too big
+            // if they guess 13 and n=13 then its just right
+        if (n < g) {
+            System.out.println("Too big");
+        }
+        if (n > g) {
+            System.out.println("Too small");
+
+        if (n == g) {
+            System.out.println("Correct Guess!!");
+        }
+         tries = tries + 1;
+            System.out.println(tries + " tries " );
+
+        }
+
+
+
+
+
     }
+
 }
+
